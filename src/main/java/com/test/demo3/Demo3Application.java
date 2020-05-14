c084b729f8d816297cc56036a1959ce776b49903
zz <584449728@qq.com>
2020-05-14 11:23:25 +0800

    hello

--- src/main/java/com/test/demo3/Demo3Application.java
+++ src/main/java/com/test/demo3/Demo3Application.java
@@ -2,12 +2,21 @@ package com.test.demo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo3Application.class, args);
+    }
+
+    @GetMapping("hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "世界") String name) {
+        return String.format("Hello %s!", name);
     }
 
 }
